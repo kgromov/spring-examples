02fa506d0bd8a67295db72ae6d9a396b55e70cf8
Konstantin <konstantin@revimedia.com>
2019-07-14 14:46:36 +0300

    start with exceptions handling

--- src/main/java/guru/springframework/controllers/ImageController.java
+++ src/main/java/guru/springframework/controllers/ImageController.java
@@ -34,15 +34,12 @@ public class ImageController {
     @GetMapping("recipe/{id}/image")
     public String showUploadForm(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-
         return "recipe/imageuploadform";
     }
 
     @PostMapping("recipe/{id}/image")
     public String handleImagePost(@PathVariable String id, @RequestParam("imagefile") MultipartFile file){
-
         imageService.saveImageFile(Long.valueOf(id), file);
-
         return "redirect:/recipe/" + id + "/show";
     }
 
